38c439e452acab9d7bfbd19e2801c1bd6d3fcd4e
WhyNotDogie <112395091+WhyNotDogie@users.noreply.github.com>
2025-08-07 15:42:32 -0700

    feat: initial creation of pack dev utilities - whynotdogie

---
+++ packdev/install.py
@@ -0,0 +1,2 @@
+# TODO: Add file installation code (for developers) (install files from modrinth.index.json)
+# IMPORTANT: CHECK FOR ILLEGAL PATHS (DO NOT ALLOW ESCAPING MINECRAFT'S PATH)
---
+++ packdev/pack.py
@@ -0,0 +1,1 @@
+# TODO: Write packaging code
